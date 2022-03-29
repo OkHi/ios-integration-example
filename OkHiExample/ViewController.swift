1f59c45e841232dc2c1bd4a8b6a2b9778782d2e3
Julius Kiano <jgkiano@gmail.com>
2022-03-29 19:12:29 +0300

    Initial Commit

---
+++ OkHiExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OkHiExample
+//
+//  Created by Julius Kiano on 29/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
